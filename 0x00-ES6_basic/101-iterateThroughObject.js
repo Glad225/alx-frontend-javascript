763972d8438f92a2504d18d935f2d7a358ae8efa
Glad225 <okorochaglad170@gmail.com>
2023-10-01 11:27:31 -0700

    frontend

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,10 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  for (const item of reportWithIterator) {
+    if (result !== '') {
+      result += ' | ';
+    }
+    result += item;
+  }
+  return result;
+}
